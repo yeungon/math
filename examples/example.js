88f7243746265ea1dc66133758e3d2bda32e378c
vuong <yeungon@gmail.com>
2022-12-20 15:55:26 +0700

    Added - addition - basic_math

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,12 @@
-import {random, basic_algebra, absolute_difference} from 'math-generator';
+import {
+	random,
+	addition,
+	basic_algebra,
+	absolute_difference,
+} from "math-generator";
 
 console.log(random(100));
 console.log(basic_algebra(100));
-console.log(absolute_difference());
+console.log(absolute_difference(96, -27));
+
+console.log(addition(100, 10));
